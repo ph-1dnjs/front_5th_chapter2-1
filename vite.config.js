201bd32d44b8a6d9d3cbe8b9e8d8b2bf7808e70b
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-17 22:19:12 +0900

    fix: 배포 테스트

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  base: isProd ? '/front_5th_chapter2-1/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
@@ -15,7 +12,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        basic: path.resolve(__dirname, 'index.basic.html'),
+        main: path.resolve(__dirname, 'index.basic.html'),
         advanced: path.resolve(__dirname, 'index.advanced.html'),
       },
     },
